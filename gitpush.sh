8935ba438ff3e35f05c000ad13b5f335f2e3938e
Logan Alldredge <one-enterprises.tech@outlook.com>
2022-07-02 15:44:52 -0600

    29

--- gitpush.sh
+++ gitpush.sh
@@ -1,34 +1,26 @@
-#!/bin/bash
+#!/bin/bash -e
 
 echo "Welcome to the GitHub File Publishing Tool (GHFP for short)"
-read -p "Continue? " yn
+
+set -e $yn Yy
+
+read -p "Continue? Default is y. " yn
 
 case $yn in
 [Yy]* ) echo "Continuing the program...";;
 [Nn]* ) echo "Stopping the program"; exit;;
 esac
 
-echo "Your remote will be shown below."
+REMOTE=$(git remote)
 
-git remote
+BRANCH=$(git remote show $REMOTE | grep 'HEAD branch' | cut -d' ' -f5)
 
-read -p "What is the remote of your repository? See above for the remote. " REMOTE
+NUM=$(git rev-list --count $BRANCH)
 
-echo "Your main branch will be shown below."
+NUM1=$(($NUM + 1))
 
-git remote show $REMOTE | sed -n '/HEAD branch/s/.*: //p'
+git add -u
 
-read -p "What branch would you like to write changes to? For example, enter master for main/default branch. " BRANCH
-
-echo "The number of commits for branch" $BRANCH "will show below."
-
-git rev-list --count $BRANCH
-
-git add .
-
-read -r -p "Commit name/number/identifier: " NUM
-
-
-git commit -m $NUM
+git commit -m $NUM1
 
 git push

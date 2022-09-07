a01e2c746fb68bf305ef177498f15cc5e559d8ea
Logan Alldredge <lalldredge7622@uinta1.org>
2022-09-07 08:33:21 -0600

    Create archclone.sh

---
+++ bash/archclone.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "This program will clone the Arch Linux repository from http://mirrors.aggregate.org/archlinux/"
+echo "\nPlease note that this program is new and in the development/testing stage."

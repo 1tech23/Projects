d9175115055ef05e50cb1c12975e3ca5fd42b10e
Logan Alldredge <one-enterprises.tech@outlook.com>
2023-01-09 10:57:44 -0700

    Create hobbyinv.sh

---
+++ bash/hobbyinv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "This executable bash script has been"
+echo "created by the One Projects,"
+echo "owned and operated by logan Alldredge"
+echo "&c Logan Alldredge"
+
+echo "                                      "
+
+echo "Be warned that yhis script"
+echo "is still an experimenbt"
+echo "and is used at your own risk."
+echo "However, this script"
+echo "should not have anything"
+echo "that changes your system."
+ehco "It is intended to function"
+echo "as an AI that generates"
+echo "a shopping list for"
+echo "a particular hobby."
+
+echo "By continuing, you"
+echo "understand the risk"
+echo "and notice that the"
+echo "proprietor of these"
+echo "resources does not accept"
+echo "responsibility for a damaged system."
+
+echo "If you wish to quit, press"
+echo "control + c at any time."

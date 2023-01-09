6285ee0627779592736c79e810adbbb1d7b0533b
Logan Alldredge <one-enterprises.tech@outlook.com>
2023-01-09 11:02:22 -0700

    Update hobbyinv.sh

--- bash/hobbyinv.sh
+++ bash/hobbyinv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ tput setaf 1; echo "PLEASE READ ALL OF THE BELOW TEXT"
+
+
 echo "This executable bash script has been"
 echo "created by the One Projects,"
 echo "owned and operated by logan Alldredge"
@@ -13,10 +16,12 @@ echo "and is used at your own risk."
 echo "However, this script"
 echo "should not have anything"
 echo "that changes your system."
-ehco "It is intended to function"
+echo "It is intended to function"
 echo "as an AI that generates"
 echo "a shopping list for"
 echo "a particular hobby."
+
+echo "                                           "
 
 echo "By continuing, you"
 echo "understand the risk"
@@ -25,5 +30,8 @@ echo "proprietor of these"
 echo "resources does not accept"
 echo "responsibility for a damaged system."
 
+echo "                                       "
+
 echo "If you wish to quit, press"
 echo "control + c at any time."
+
